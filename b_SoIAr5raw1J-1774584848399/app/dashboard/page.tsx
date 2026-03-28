fa4420b59ad0aef1d39537353e031d6b6e5a276b
Tejxs-12 <tejask1204@gmail.com>
2026-03-28 10:28:34 +0530

    Updated API URL for deployment

--- b_SoIAr5raw1J-1774584848399/app/dashboard/page.tsx
+++ b_SoIAr5raw1J-1774584848399/app/dashboard/page.tsx
@@ -53,7 +53,7 @@ export default function DashboardPage() {
     setAnalysisResult(null)
 
     try {
-      const res = await fetch("http://localhost:5000/predict", {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
